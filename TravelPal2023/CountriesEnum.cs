3ccc2365b167ef0e2d36dd5a04463fa60addc946
Sjukparkering <danielsupina@hotmail.com>
2023-10-25 13:55:45 +0200

    Initial commit

---
+++ TravelPal2023/CountriesEnum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPal2023
+{
+    internal enum Country
+    {
+        France,
+        USA,
+        Spain,
+        China,
+        Italy,
+        United_Kingdom,
+        Germany,
+        Mexico,
+        Thailand,
+        Turkey,
+        Austria,
+        Malaysia,
+        Hong_Kong,
+        Greece,
+        Russia,
+        Japan,
+        Canada,
+        Saudi_Arabia,
+        Poland,
+        South_Korea,
+        Netherlands,
+        Macao,
+        Hungary,
+        United_Arab_Emirates,
+        India,
+        Croatia,
+        Ukraine,
+        Singapore,
+        Indonesia,
+        Czech_Republic,
+
+
+    }
+}
---
+++ TravelPal2023/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPal2023
+{
+    public interface IUser
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public Country country { get; set; }
+    }
+}
